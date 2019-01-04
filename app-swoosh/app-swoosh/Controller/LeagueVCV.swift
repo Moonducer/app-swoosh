32ee99e8d4e6a411066e6546f231e5d681ac86fd
Moonducer <tomasz.guja@icloud.com>
2019-01-04 17:23:06 +0100

    refactoring

---
+++ app-swoosh/app-swoosh/Controller/LeagueVCV.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVCV.swift
+//  app-swoosh
+//
+//  Created by Tomasz Guja on 04/01/2019.
+//  Copyright © 2019 Tomasz Guja. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVCV: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
