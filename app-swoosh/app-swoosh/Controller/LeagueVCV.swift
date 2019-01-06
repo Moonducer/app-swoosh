f3c081bf00f082ae7b05a2972bf2b5db41ce8205
Moonducer <tomasz.guja@icloud.com>
2019-01-06 09:35:26 +0100

    new model for player and enabling Next button

--- app-swoosh/app-swoosh/Controller/LeagueVCV.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVCV.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class LeagueVCV: UIViewController {
+    
+    var player: Player!
+    
 
-   
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
         // Do any additional setup after loading the view.
     }
@@ -30,6 +36,24 @@ class LeagueVCV: UIViewController {
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Tomasz Guja on 06/01/2019.
+//  Copyright © 2019 Tomasz Guja. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague : String?
+    var sellectedSkillLevel : String?
+}
